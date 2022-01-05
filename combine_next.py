58e6a6fa4edc0035d78303c491cadd36b27ec51b
Cam Allen <csal@brown.edu>
2022-01-05 02:50:13 -0500

    Update combine scripts

--- combine_next.py
+++ combine_next.py
@@ -2,16 +2,13 @@ import glob
 import json
 from tqdm import tqdm
 
-all_results = {}
-file_pattern = 'results/*'
+nextWord = {}
+file_pattern = 'next-results/*'
 filepaths = sorted(glob.glob(file_pattern))
 len(filepaths)
 for filepath in tqdm(filepaths):
     with open(filepath, 'r') as fp:
         results = json.load(fp)
-    all_results.update(results)
+    nextWord.update(results)
 
-bestWord = min(all_results, key=all_results.get)
-bestWord, all_results[bestWord]
-
-# json.dumps(all_results)
+nextWord
